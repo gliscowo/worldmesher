cd7686b3914515eebf58dc4eb372134718d41708
parzivail <colby.newman2000@gmail.com>
2022-11-11 11:07:15 -0500

    rename WorldMesh::reset to WorldMesh::clear, add javadoc

--- src/main/java/io/wispforest/worldmesher/WorldMesh.java
+++ src/main/java/io/wispforest/worldmesher/WorldMesh.java
@@ -156,7 +156,12 @@ public class WorldMesh {
         return dimensions;
     }
 
-    public void reset() {
+    /**
+     * Release unmanaged vertex buffers and clear all initialized
+     * layers. Resulting mesh state is ready to accept new vertex
+     * data.
+     */
+    public void clear() {
         this.bufferStorage.forEach((renderLayer, vertexBuffer) -> vertexBuffer.close());
         this.bufferStorage.clear();
 
